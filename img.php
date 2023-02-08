c0085f4a1e0ca1b309df7b1293ae2299be3d956e
Oliver <38598074+Stromic@users.noreply.github.com>
2023-02-09 00:02:08 +0100

    Simplified, bug fix
    Removed the need for a secret key, MySQL Events and fixed a few bugs.
    
    Now you only need to upload this PHP file and set the DB up and update the url in-game!

--- img.php
+++ img.php
@@ -1,71 +1,66 @@
 <?php
-$dbhost = 'localhost';
-$dbname = '';
-$dbusername = '';
-$dbpassword = '';
-$secretkey = '';
+    // MySQL Config
 
-$b64 = $_POST['b64'];
-$imgid = $_GET['id'];
+    $dbhost = 'localhost';
+    $dbname = '';
+    $dbusername = '';
+    $dbpassword = '';
+    
+    // End of config
 
-try {
-  $dbcon = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbusername, $dbpassword);
-  $dbcon->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-} catch(PDOException $e){
-  die();
-}
-
-if ($imgid && $b64 || !$imgid && !$b64) {
-  $secret = $_GET['secret'];
-  if (isset($secret) && $secret == $secretkey){
-    $curtime = time();
-    $request = $dbcon->query("SELECT * FROM images");
-    $result = $request->fetchAll(PDO::FETCH_ASSOC);
-
-    foreach($result as $image) {
-      if ($curtime - $image['created'] > 10) {
-        $loopid = $image['id'];
-        $request = $dbcon->prepare("DELETE FROM images WHERE id = '$loopid'")->execute();
+    $b64 = $_POST['b64'];
+    $imgid = $_GET['id'];
+    
+    function generateRandomString($length = 10) {
+      $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      $charactersLength = strlen($characters);
+      $randomString = '';
+      for ($i = 0; $i < $length; $i++) {
+          $randomString .= $characters[rand(0, $charactersLength - 1)];
       }
+      return $randomString;
     }
-  }
-  die();
-}
 
-function generateRandomString($length = 10) {
-  $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  $charactersLength = strlen($characters);
-  $randomString = '';
-  for ($i = 0; $i < $length; $i++) {
-      $randomString .= $characters[rand(0, $charactersLength - 1)];
-  }
-  return $randomString;
-}
-if (isset($b64)) {
-  $ip = $_SERVER['REMOTE_ADDR'];
-  $request = $dbcon->query("SELECT b64 FROM images WHERE ip = '$ip'");
-  $ratelimit = $request->fetch();
-  
-  if ($ratelimit) {
-    die();
-  }
-  
-  $id = generateRandomString();
-  $sql = "INSERT INTO images (id, b64, created, ip) VALUES (?,?,?,?)";
-  $dbcon->prepare($sql)->execute([$id, base64_decode($b64, TRUE), time(), $ip]);
-
-  echo($id);
-} if (isset($imgid)){
-  $request = $dbcon->query("SELECT b64 FROM images WHERE id = '$imgid'");
-  $img = $request->fetch();
-
-  $request = $dbcon->prepare("DELETE FROM images WHERE id = '$imgid'")->execute();
-  $b64data = $img['b64'];
-  
-  if (!$b64data) {
-    die();
-  }
-
-  echo(base64_encode($b64data));
-}
+    try {
+      $dbcon = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbusername, $dbpassword);
+      $dbcon->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    } catch(PDOException $e){
+      die();
+    }
+    
+    // Some hosts doesnt allow SQL Events, this means that we have to resort to some hacky solutions such as this one.
+    // We clear the database everytime someone uploads an image.
+    $dbcon->query("DELETE FROM images WHERE UNIX_TIMESTAMP() - created > 10");
+    
+    // Upload image and return the img_id.
+    if (isset($b64)) {
+      $ip = $_SERVER['REMOTE_ADDR'];
+      $request = $dbcon->query("SELECT b64 FROM images WHERE ip = '$ip'");
+      $ratelimit = $request->fetch();
+      
+      if ($ratelimit) {
+        die();
+      }
+      
+      $img_id = generateRandomString();
+      $sql = "INSERT INTO images (img_id, b64, created, ip) VALUES (?,?,?,?)";
+      $dbcon->prepare($sql)->execute([$img_id, base64_decode($b64, TRUE), time(), $ip]);
+    
+      echo($img_id);
+    }
+    
+    // Return the image if the img_id exists, delete after it has been sent to the requester.
+    if (isset($imgid)){
+      $request = $dbcon->query("SELECT b64 FROM images WHERE img_id = '$imgid'");
+      $img = $request->fetch();
+    
+      $request = $dbcon->prepare("DELETE FROM images WHERE img_id = '$imgid'")->execute();
+      $b64data = $img['b64'];
+      
+      if (!$b64data) {
+        die();
+      }
+    
+      echo(base64_encode($b64data));
+    }
 ?>
